2ac2bf554a20cf83d2be5dd42ddc450765358e59
“MsiAr” <snowflaker6090@gmail.com>
2025-05-30 22:52:45 +0530

    completed the layout of homepage and added links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn={firstName:'Dev', lastName:'Arpit', email:'snowflaker6090@gmail.com'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName||'User'}
+            subtext='Here’s what’s happening with your account today'
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={150}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+        user={loggedIn} 
+        transactions={[]}
+        banks={[{currentBalance:'123'},{currentBalance:'456'}]}
+      />
+    </section>
   )
 }
 
